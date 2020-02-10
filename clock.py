5fb47f2f030465574b0db2f2f387dd5e001263ac
Mihai Starniciuc <Mihai.Starniciuc@endava.com>
2020-02-10 23:34:30 +0200

    test

--- clock.py
+++ clock.py
@@ -8,42 +8,9 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 update_id = None
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=21, minute=30)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=21, minute=36)
 def scheduled_job():
-    """Run the bot."""
-    global update_id
-    # Telegram Bot Authorization Token
     bot = telegram.Bot('1083569431:AAH0uZC0dvSndasusLN_c5szzvgTKUMjEak')
-
-    # get the first pending update_id, this is so we can skip over it in case
-    # we get an "Unauthorized" exception.
-    try:
-        update_id = bot.get_updates()[0].update_id
-    except IndexError:
-        update_id = None
-
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    while True:
-        try:
-            echo(bot)
-        except NetworkError:
-            sleep(1)
-        except Unauthorized:
-            # The user has removed or blocked the bot.
-            update_id += 1
-
-
-def echo(bot):
-    """Echo the message the user sent."""
-    global update_id
-    # Request updates after the last update_id
-    for update in bot.get_updates(offset=update_id, timeout=10):
-        update_id = update.update_id + 1
-
-        if update.message:  # your bot can receive updates without messages
-            # Reply to the message
-            update.message.reply_text('12:00?')
-
+    bot.send_message('12:00?')
 
 sched.start()
