0d79ed54ce8e8f5c0dfcfb226b6e477758beee78
Mihai Starniciuc <mihai.starniciuc@endava.com>
2020-02-11 09:30:39 +0200

    name

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ update_id = None
 
 schedule_lanch = [
     "*Italian Lunch*\n - Supa Minestrone \n - Paste Bolognese", 
-    "*Moldovan Luuch*\n - Zeama \n - Mamaliga cu tocana de pui", 
+    "*Moldovan Lunch*\n - Zeama \n - Mamaliga cu tocana de pui", 
     "*Indian Lunch*\n - Supa cu somon \n - File de pui cu sos curry si orez", 
     "*American Lunch*\n - Supa crema de ciuperci \n - Costita de porc cu sos BBQ si cartofi copti", 
     "*Mexican Lunch*\n - Supa picanta de fasole \n - Fajitas de vita cu legume", 
