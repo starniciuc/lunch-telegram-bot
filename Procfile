8e30bec6e4dc550e7b54b4a02fda63889c602c7b
Mihai Starniciuc <mihai.starniciuc@endava.com>
2020-02-11 09:36:52 +0200

    set cron at 11.30

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 worker: python3 bot.py 8443
+clock: python3 clock.py
--- clock.py
+++ clock.py
@@ -8,9 +8,9 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 update_id = None
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=21, minute=45)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=9, minute=30)
 def scheduled_job():
     bot = telegram.Bot('1083569431:AAH0uZC0dvSndasusLN_c5szzvgTKUMjEak')
-    bot.send_message(chat_id='50964383', text="12:00?")
+    bot.send_message(chat_id='-208886819', text="12:00?")
 
 sched.start()
